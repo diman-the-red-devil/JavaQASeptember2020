6976b1c7332b7ea192c5bb88fda613de9fab7f8b
diman_the_red_devil <diman_the_red_devil@mail.ru>
2020-10-17 22:10:39 +0400

    Add enum Browser

---
+++ tests-junit/src/test/java/Browser.java
@@ -0,0 +1,15 @@
+public enum Browser {
+    CHROME("chrome"),
+    FIREFOX("firefox"),
+    IE("ie");
+
+    private String browserName;
+
+    Browser(String browserName) {
+        this.browserName = browserName;
+    }
+
+    public String getBrowserName() {
+        return browserName;
+    }
+}
