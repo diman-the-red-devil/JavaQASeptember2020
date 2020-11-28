1d4be1783441b42b676c7b9d53297d440d0bf934
diman-the-red-devil <diman_the_red_devil@mail.ru>
2020-11-29 03:41:15 +0400

    Edit ServerConfig.java

--- owner/src/main/java/config/ServerConfig.java
+++ owner/src/main/java/config/ServerConfig.java
@@ -5,6 +5,6 @@ import org.aeonbits.owner.Config.Sources;
 
 @Sources("classpath:config.properties")
 public interface ServerConfig extends Config {
-    @Key("url")
-    String url();
+    @Key("otus_url")
+    String otus_url();
 }
